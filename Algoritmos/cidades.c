a8d8a53f295aa4f7a08796cd244ff895636810f5
diogo200913 <diogo.joao.1999@gmail.com>
2018-03-08 11:17:03 +0000

    Similar countries

--- Algoritmos/cidades.c
+++ Algoritmos/cidades.c
@@ -18,9 +18,11 @@ vetor* cidades_load(const char *nomef)
   FILE* ficheiro = fopen(nomef,"rb");
   if(ficheiro == NULL) return NULL;
   //load dos dados
-  cidade load_buffer;
-  while(feof(ficheiro)&&fread(&load_buffer,sizeof(cidade),1,ficheiro)){
+  while(!feof(ficheiro)){
+    cidade load_buffer;
+    if(fread(&load_buffer,sizeof(cidade),1,ficheiro)==1){
       vetor_insere(buffer,load_buffer,-1);
+     }
   }
   //return sucesso
   fclose(ficheiro);
@@ -64,19 +66,17 @@ int cidades_peek(const char *nomef, const char *nomecidade, cidade *resultado)
 }
 
 int cidades_poke(const char *nomef, const char *nomecidade, cidade nova)
-{
-  
-  int pos = cidades_peek(nomef,nomecidade,NULL);
-  
+{  
+  int pos = cidades_peek(nomef,nomecidade,NULL);  
   
   if(pos>=0)
   {
     size_t tamanho = sizeof(cidade); 
-    FILE * ficheiro = fopen(nomef,"rb+");
+    FILE * ficheiro = fopen(nomef,"wb");
     if(ficheiro==NULL) return -1;
 
     //escrita no ficheiro, na pos encontrada
-    fseek(ficheiro,pos*(tamanho),SEEK_SET);
+    fseek(ficheiro,-tamanho,SEEK_CUR);
     
     if(fwrite(&nova,tamanho,1,ficheiro)!=1) return -1;
     fclose(ficheiro);
@@ -86,10 +86,51 @@ int cidades_poke(const char *nomef, const char *nomecidade, cidade nova)
 
 int cidades_resort(vetor *vec, char criterio)
 {
+  
   return -1;
 }
 
 char** cidades_similar (vetor *vec, const char *nomecidade, int deltapop, int *nsimilares)
 {
-  return NULL;
+  if (vec==NULL || nomecidade==NULL) return NULL;
+
+  int posref=-1;
+
+  int i;
+  for(i=0; i<vec->tamanho ; i++){
+    if(strcmp(nomecidade,vec->elementos[i].nome)==0){
+      posref=i;
+      break;
+    }
+  }
+
+  if(posref==-1) return NULL;
+
+  int popref=vec->elementos[posref].populacao;
+
+  char **vecsimilares;
+
+  vecsimilares=malloc(sizeof(char**));
+  vecsimilares[0]=calloc(MAXSTR,sizeof(char*));
+
+  (*nsimilares)=0;
+  
+  for(i=0; i<vec->tamanho ; i++){
+    
+    if((vec->elementos[i].populacao - popref)<=deltapop && (vec->elementos[i].populacao - popref)>= -1*deltapop && strcmp(vec->elementos[i].nome,nomecidade)!=0) {
+      
+      if(nsimilares!=0){
+        vecsimilares=realloc(vecsimilares,((*nsimilares)+1)*sizeof(char*));
+        vecsimilares[*nsimilares]=calloc(MAXSTR,sizeof(char*));  
+      }
+
+      strcpy(vecsimilares[*nsimilares],vec->elementos[i].nome);
+      (*nsimilares)++;
+
+    }
+  }
+
+  if((*nsimilares)==0) return NULL;
+
+  return vecsimilares;
 }
