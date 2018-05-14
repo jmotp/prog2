6e1010f56835915fd2054d7e6cd9fcfa96aaf025
João Pinheiro <jmotp@live.com.pt>
2018-05-14 18:14:07 +0100

    Start

--- Terra_Incognita/main.c
+++ Terra_Incognita/main.c
@@ -11,13 +11,45 @@ int mapa_pos(int x, int y)
 
 int main(int argc, char *argv[])
 {
+      int nExplorador;
+      int positio[1000][2];
     /* 1) iniciar comunicacao com exploradores
           NOTA: primeiros parametros deverao ser argc e argv */
-
-
+      
+      intro(argc,argv,&nExplorador,positio);
+      realloc(positio,nExplorador * 2 * sizeof(int));
+      printf("%d\n",nExplorador);
+      for(int i = 0  ; i < nExplorador; i++){
+            printf("%d %d\n",positio[i][0],positio[i][1]);
+      }
     /* 2) comunicar com os exploradores e receber informacoes, 
           enquanto existem movimentacoes a realizar */
-
+      int id,typus;
+      char save;
+      int minh = 1000000, maxh= -1000000, maxv = -10000000, minv = 1000000;
+      while('X'!=(save=explorator(&id,&typus))){
+            printf("%c %d %d \n",save,id,typus);
+            switch(save){
+                  case 'N':
+                        positio[id][1]--;
+                        break;
+                  
+                  case 'S':
+                        positio[id][1]++;
+                        break;
+                  case 'O':
+                        positio[id][0]--;
+                        break;
+                  case 'E':
+                        positio[id][0]++;
+                        break;                
+            }
+            if(positio[id][0]<minh)minh = positio[id][0];
+            if(positio[id][0]>maxh)maxh = positio[id][0];
+            if(positio[id][1]<minv)minv = positio[id][1];
+            if(positio[id][1]>maxv)maxv = positio[id][1];
+      };
+      printf("%d %d\n",maxh-minh +1 ,maxv-minv +1 );
 
     /* 3) imprime e verifica o mapa
           NOTA: funcao mapa_pos e' o primeiro das funcoes */
