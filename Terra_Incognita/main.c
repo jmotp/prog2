d0bd4f08c614276eb54dee318385210205683b66
João Pinheiro <jmotp@live.com.pt>
2018-05-14 14:01:15 +0100

    Initial commit Terra_Incognita

---
+++ Terra_Incognita/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "terra_incognita.h"
+
+int mapa_pos(int x, int y)
+{
+    /* devolve tipo de terreno na posicao x,y */
+    
+    return TERRA_INCOGNITA;
+}
+
+int main(int argc, char *argv[])
+{
+    /* 1) iniciar comunicacao com exploradores
+          NOTA: primeiros parametros deverao ser argc e argv */
+
+
+    /* 2) comunicar com os exploradores e receber informacoes, 
+          enquanto existem movimentacoes a realizar */
+
+
+    /* 3) imprime e verifica o mapa
+          NOTA: funcao mapa_pos e' o primeiro das funcoes */
+
+
+    /* 4) termina comunicacoes com os exploradores */
+
+
+    return 0;
+}
